3a610c61c01ca60811ff87423a71f2cc7755014f
bangnn.os <bangnn.os@mbbank.com>
2024-05-25 22:43:10 +0700

    confic csrf

--- src/main/java/com/udemy/security/config/SecurityConfig.java
+++ src/main/java/com/udemy/security/config/SecurityConfig.java
@@ -1,28 +1,73 @@
 package com.udemy.security.config;
 
+import com.udemy.security.filter.CsrfCookieFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf.disable())
-                .authorizeHttpRequests((requests) -> requests
-//                        .requestMatchers("/api/v1/**").authenticated()
-                        .requestMatchers("/myAccount", "/myBalance", "/myCard", "/myLoans", "/user").authenticated()
-//                        .requestMatchers("/notices", "/contact", "/register").permitAll())
-                        .requestMatchers("/myNotice", "/myContact", "/register").permitAll())
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
+        http.securityContext(securityContext -> securityContext.requireExplicitSave(false))
+                .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+//                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);  // Use uppercase L for long literal
+                        return config;
+                    }
+                }))
+                .csrf(csrf -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers( "/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                ).addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/myNotice","/myAccount","/myContact", "/myBalance", "/myCard", "/myLoans", "/user").authenticated()
+                        .requestMatchers( "/register").permitAll()
+                )
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
+
         return http.build();
     }
+
+
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+//        configuration.setAllowedHeaders(Arrays.asList("*"));
+//        configuration.setMaxAge(3600l);
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
